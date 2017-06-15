3276683a697dc95fe0fece35fad9ee51ee41fe77
Sergey <sergeykhval@gmail.com>
2017-06-15 23:52:44 +0300

    feat: add recipes sidebar and basic ability to add new recipes

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getRecipes } from '../../reducer';
+import { addRecipe as addRecipeAction } from '../../actions';
 import './style.css';
 
-function Sidebar() {
-  return (
-    <div className="sidebar">Sidebar</div>
-  );
+class Sidebar extends Component {
+  state = {
+    title: '',
+    ingridients: '',
+  };
+
+  handleAddRecipe(e) {
+    e.preventDefault();
+    this.props.addRecipe(this.state);
+    this.setState({ title: '', ingridients: '' });
+  }
+
+  render() {
+    const { recipes } = this.props;
+
+    return (
+      <div className="sidebar">
+        {recipes.map(r => <p>{r.title}</p>)}
+
+        <form onSubmit={::this.handleAddRecipe}>
+          <p>
+            <input value={this.state.title} onChange={e => this.setState({ title: e.target.value })} type="text" />
+          </p>
+          <p>
+            <input
+              value={this.state.ingridients}
+              onChange={e => this.setState({ ingridients: e.target.value })}
+              type="text"
+            />
+          </p>
+          <button>Add Recipe</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Sidebar;
+function mapStateToProps(state) {
+  return {
+    recipes: getRecipes(state),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addRecipe: recipe => dispatch(addRecipeAction(recipe)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,25 @@
-const defaultRecipes = {
-  1: {
+import { ADD_RECIPE } from './actions';
+
+const defaultRecipes = [
+  {
     title: 'fish',
     ingridients: '',
   },
-  2: {
+  {
     title: 'meat',
     ingridients: '',
   },
-};
+];
 
-function recipesReducer(recipes = defaultRecipes, action) {
-  switch (action.type) {
+function recipesReducer(recipes = defaultRecipes, { type, payload }) {
+  switch (type) {
+    case ADD_RECIPE:
+      return [...recipes, payload];
     default:
       return recipes;
   }
 }
 
 export default recipesReducer;
+
+export const getRecipes = ({ recipes }) => recipes;
