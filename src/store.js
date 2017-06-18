0b3dcd9f0ee2ec7337de39c40991bf3a8af0d5dd
Sergey <sergeykhval@gmail.com>
2017-06-18 21:21:41 +0300

    feat: add ability to persist state to localstorage

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
+import throttle from 'lodash/throttle';
 import { combineReducers, createStore } from 'redux';
 import recipes from './reducer';
+import { loadState, saveState } from './localStorage';
 
 const rootReducer = combineReducers({ recipes });
+const persistedState = loadState();
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(throttle(() => saveState(store.getState()), 1000));
 
 export default store;
